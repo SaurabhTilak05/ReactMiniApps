7ba1fbbc000a37894c0edbf2d0c0cc4f776f23f0
saurabh <saurbhtilak6@gmail.com>
2025-07-28 11:21:42 +0530

    This is home page to display the options

--- ExamApplication/src/HomePage.jsx
+++ ExamApplication/src/HomePage.jsx
@@ -1,13 +1,29 @@
 import React from "react";
-import ReactDom from "react-dom";
+import { Link } from "react-router-dom";
 
-class Home extends React.Component{
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="container mt-5 text-center">
+        <div className="p-5 bg-primary text-white rounded shadow">
+          <h1 className="mb-3">🎓 Welcome to Exam App</h1>
+          <p className="lead">Prepare, Practice, and Test your knowledge!</p>
 
-    render() {
-      return <>
-      
-      <h1>Hii Good Morning</h1>
-      </>
-    }
+          <div className="mt-4">
+            <Link to="/addQuestion" className="btn btn-light m-2">
+              ➕ Add Question
+            </Link>
+            <Link to="/viewQuestion" className="btn btn-light m-2">
+              📋 View Questions
+            </Link>
+            <Link to="/AttendExam" className="btn btn-success m-2">
+              📝 Attend Exam
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
 export default Home;
